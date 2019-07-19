5bd0aa3d650a72154fda9d3297d3b2d80a9cb3cc
NegohPreto <jgabrieltomaz2706@gmail.com>
2019-07-18 21:46:15 -0300

    Altera o layout

---
+++ JS/main.js
@@ -0,0 +1,10 @@
+function Chamar_Sobre(){
+    document.getElementById('main_loopis').style = "display: none";
+    document.getElementById('main_cards').style = "display: none";
+    document.getElementById('main_sobre').style = "display: block";
+}
+function Chamar_Home(){
+    document.getElementById('main_loopis').style = "display: block";
+    document.getElementById('main_cards').style = "display: block";
+    document.getElementById('main_sobre').style = "display: none";
+}
